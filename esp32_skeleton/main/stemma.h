895b420fb4aa35679aef1109bb3af9c02696b173
jiftesam <143129435+jiftesam@users.noreply.github.com>
2025-09-28 12:00:18 -0400

    Adding Drivers

---
+++ esp32_skeleton/main/stemma.h
@@ -0,0 +1,10 @@
+// stemma.h - STEMMA sensor driver header (stub)
+#ifndef STEMMA_H
+#define STEMMA_H
+
+// Initializes the STEMMA sensor
+void stemma_init(void);
+
+// INCLUDE THE REST OF THE FUNCTIONS BELOW
+
+#endif // STEMMA_H
